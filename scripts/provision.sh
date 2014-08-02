994ddba576e6d41320d7fad9da594883b6cc6def
Tom Jenkinson <tjcode@outlook.com>
2014-08-02 20:23:27 +0100

    Provision script now sets "client_max_body_size" in nginx config.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -8,9 +8,13 @@ php artisan migrate --seed
 echo "Setting 'upload_max_filesize' and 'post_max_size' in 'php.ini'."
 CONFIG_FILE="/etc/php5/fpm/php.ini"
 VAL="1000M"
-sed -i "s/\(upload_max_filesize *= *\).*/\1$VAL/" $CONFIG_FILE
-sed -i "s/\(post_max_size *= *\).*/\1$VAL/" $CONFIG_FILE
-echo "Restarting php."
+sed -i "s/\(upload_max_filesize *= *\).*/\1$VAL/" "$CONFIG_FILE"
+sed -i "s/\(post_max_size *= *\).*/\1$VAL/" "$CONFIG_FILE"
+echo "Setting 'client_max_body_size' for nginx."
+CONFIG_FILE="/etc/nginx/conf.d/upload_size.conf"
+VAL="1000m"
+echo "client_max_body_size $VAL;" > "$CONFIG_FILE"
+echo "Restarting nginx."
 service php5-fpm restart
 cd "$HOME"
 echo "Finished custom setup script."
