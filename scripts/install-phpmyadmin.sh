63e471d313b4a4fb25b6bf187e392d4f775ae8fc
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-08-11 21:20:08 +0100

    removed pointless file

--- scripts/install-phpmyadmin.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "Installing phpMyAdmin..."
-cd "/home/vagrant"
-rm -rf "phpMyAdmin"
-git clone https://github.com/phpmyadmin/phpmyadmin.git "phpMyAdmin"
-
-cd "$HOME"
-echo "Installed phpMyAdmin."
