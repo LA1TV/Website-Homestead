f6337c85a9bab96ab9b6d4d6ced18b66152c33d5
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-08-17 11:30:46 +0100

    Added help message to end of provision script.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -26,3 +26,7 @@ service nginx restart
 service php5-fpm start
 cd "$HOME"
 echo "Finished custom setup script."
+echo "-------------------------------"
+echo "You should now be able to access the website at 'http://local.www.la1tv.co.uk:8000/' (provided that you've edited your hosts file) and phpMyAdmin at 'http://local.www.la1tv.co.uk:3406/'."
+echo "You can ssh into the box with 'vagrant ssh' and the login for the database is 'homestead' and 'secret'."
+echo "The command to shutdown the virtual machine is 'vagrant halt' and 'vagrant suspend' will suspend it."
