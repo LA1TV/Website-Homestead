35d3fbe8c54de642514e1f4cbc1f00617768c4f2
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2014-10-07 16:04:54 +0100

    Provision script now creates file_chunks directory.

--- scripts/create-storage-directories.sh
+++ scripts/create-storage-directories.sh
@@ -4,6 +4,7 @@ cd "/home/vagrant/Code/LA1-Website"
 mkdir app/storage
 mkdir app/storage/cache
 mkdir app/storage/files
+mkdir app/storage/file_chunks
 mkdir app/storage/logs
 mkdir app/storage/meta
 mkdir app/storage/pending_files
